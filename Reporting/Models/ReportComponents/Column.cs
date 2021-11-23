bc53723c7afdcb1f745627e7e34f6ced82d1977b
David Landes <dlandes.develop@outlook.com>
2021-11-23 14:58:13 -0500

    Add column report component

---
+++ Reporting/Models/ReportComponents/Column.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reporting.Models.Html;
+
+namespace Reporting.Models.ReportComponents
+{
+    class Column : HtmlTagBase
+    {
+        #region Fields
+
+        private string _width;
+
+        #endregion Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// A column of html elements.
+        /// </summary>
+        /// <param name="width">Width of the column using html sizing format</param>
+        /// <param name="items">Html elements to put inside the column</param>
+        public Column(string width = "90%", params HtmlTagBase[] items)
+        {
+            _tag = "table";
+            _width = width;
+
+            // Set the table width & center it horizontally.
+            AddAttribute("style", $"width: {_width}; margin: 0 auto;");
+
+            // Add data into the column by creating rows and stacking them on top of each other.
+            foreach (HtmlTagBase tag in items)
+            {
+                // Create the html row container.
+                Tag rowContainer = new Tag("tr");
+                rowContainer.AddAttribute("style", $"width: 100%;");
+
+                Tag rowData = new Tag("td");
+                rowData.AddAttribute("style", "text-align: center;");
+                rowData.AddContent(tag);
+                rowContainer.AddContent(rowData);
+
+                // Add the row to the table.
+                _content.Add(rowContainer);
+            }
+        }
+
+        #endregion Constructors
+    }
+}
--- Reporting/Program.cs
+++ Reporting/Program.cs
@@ -43,13 +43,13 @@ namespace Reporting
             HeaderText title = new HeaderText("Bowman and Landes Turkeys Inc.", 1);
             Text address = new Text("6490 Ross Rd. New Carlisle, OH 45344");
             Text phone = new Text("(937) 845-9466");
-            Row row = new Row("90%", title);
+
+            Row row = new Row("90%", title, title);
             Row row2 = new Row("90%", address);
             Row row3 = new Row("90%", phone);
+            Column headColumn = new Column("90%", row, row2, row3);
 
-            html.AddContent(row);
-            html.AddContent(row2);
-            html.AddContent(row3);
+            html.AddContent(headColumn);
             html.AddContent(table);
 
             string OUT_FILE = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test-report.html";
